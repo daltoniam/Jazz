36e0d9569bb3a7775cb474c010f89c8c18e8ad60
Dalton Cherry <daltoniam@gmail.com>
2015-03-12 09:59:09 -0700

    framework up yo

---
+++ Jazz.h
@@ -0,0 +1,19 @@
+//
+//  Jazz.h
+//  Jazz
+//
+//  Created by Dalton Cherry on 3/12/15.
+//  Copyright (c) 2015 Vluxe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jazz.
+FOUNDATION_EXPORT double JazzVersionNumber;
+
+//! Project version string for Jazz.
+FOUNDATION_EXPORT const unsigned char JazzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jazz/PublicHeader.h>
+
+
